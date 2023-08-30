c102773b04b6c283788243d49be5fe6e0b95db89
RaquelSAyala <142960772+RaquelSAyala@users.noreply.github.com>
2023-08-29 21:07:10 -0500

    Update OperacionesParaVectoresYMatrices.py

--- OperacionesParaVectoresYMatrices.py
+++ OperacionesParaVectoresYMatrices.py
@@ -69,13 +69,13 @@ mat2 = [[2-1j, 4+3j], [1-2j, 5+4j]]
 mat3 = [[1+2j, 3-1j], [0+1j, -2-3j]]
 
 # Pruebas para las funciones de operaciones con números complejos
-print("Suma de vectores complejos:", add_complex_vectors(vec1, vec2))
-print("Inverso de vector complejo:", inverse_complex_vector(vec1))
-print("Multiplicación escalar de vector complejo:", scalar_multiply_complex_vector(2+3j, vec1))
+print("Suma de vectores complejos:", sumar_vectores_complejos(vec1, vec2))
+print("Inverso de vector complejo:", inverso_vector_complejo(vec1))
+print("Multiplicación escalar de vector complejo:", multiplicar_escalar_vector_complejo(2+3j, vec1))
 
-print("Suma de matrices complejas:\n", add_complex_matrices(mat1, mat2))
-print("Inversa de matriz compleja:\n", inverse_complex_matrix(mat1))
-print("Multiplicación escalar de matriz compleja:\n", scalar_multiply_complex_matrix(2-1j, mat2))
+print("Suma de matrices complejas:\n", sumar_matrices_complejas(mat1, mat2))
+print("Inversa de matriz compleja:\n", inversa_matriz_compleja(mat1))
+print("Multiplicación escalar de matriz compleja:\n", multiplicar_escalar_matriz_compleja(2-1j, mat2))
 
-print("Matriz transpuesta:\n", transpose_matrix(mat3))
-print("Matriz conjugada:\n", conjugate_matrix(mat3))
+print("Matriz transpuesta:\n", matriz_transpuesta(mat3))
+print("Matriz conjugada:\n", matriz_conjugada(mat3))
